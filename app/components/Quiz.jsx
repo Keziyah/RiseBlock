32f7140f35194b8150ea77a12eed647c90d06327
Keziyah <keziyahlewis@gmail.com>
2017-06-25 21:09:11 -0400

    comments

--- app/components/Quiz.jsx
+++ app/components/Quiz.jsx
@@ -5,9 +5,9 @@ export default class Quiz extends Component {
         super(props)
 
         this.state = {
-            selected: "",
-            submitted: false,
-            correct: false
+            selected: "",  //Which option has been selected
+            submitted: false, //Has an answer been submitted
+            correct: false   //Is the answer correct
         }
 
         this.radio = this.radio.bind(this)
@@ -15,17 +15,18 @@ export default class Quiz extends Component {
         this.handleReset = this.handleReset.bind(this)
     }
 
+    //When a user makes a selection, set the state
     radio(e) {
-        this.setState({ selected: e.target.value }, () => {
-            console.log(this.state)
-        })
+        this.setState({ selected: e.target.value })
     }
 
+    //When the user makes a submition, check if it's correct, then set the state. 
     handleClick() {
         this.setState({ submitted: true })
         this.state.selected === this.props.data.answer1 ? this.setState({ correct: true }) : this.setState({ correct: false })
     }
 
+    //When the user clicks "try again", reset the state
     handleReset() {
         this.setState({submitted: false, correct: false, selected: ""})
     }
@@ -35,16 +36,21 @@ export default class Quiz extends Component {
             <div className="container">
                 <div className="quiz">
                     <div className="quiz-content">
+                        {/*{Question and image}*/}
                         <div className="quiz-content__question">
                             <p>{this.props.data.question}</p>
                             <img src={this.props.data.imgsrc} />
                         </div>
 
+                        {/*{Answers}*/}
                         <div className="quiz-content__answers">
                             <form>
-                                <label>
+
+                                <label> {/*{Do the animated border when the user submits their answer}*/}
                                     <div className={`${this.state.submitted && this.state.selected === this.props.data.answer1 ? 'quiz-content__answers-single--selected' : ""} quiz-content__answers-single`}>
                                         <input type="radio" name="answer" value={this.props.data.answer1} onClick={this.radio} />
+
+                                        {/*{Toggle the radio button}*/}
                                         {
                                             this.state.selected === this.props.data.answer1 ? 
                                             <i className="material-icons">radio_button_checked</i> :
@@ -53,9 +59,12 @@ export default class Quiz extends Component {
                                         <p>{this.props.data.answer1}</p>
                                     </div>
                                 </label>
-                                <label>
+
+                                <label> {/*{Do the animated border when the user submits their answer}*/}
                                     <div className={`${this.state.submitted && this.state.selected === this.props.data.answer2 ? 'quiz-content__answers-single--selected' : ""} quiz-content__answers-single`}>
                                         <input type="radio" name="answer" value={this.props.data.answer2} onClick={this.radio} />
+
+                                        {/*{Toggle the radio button}*/}
                                         {
                                             this.state.selected === this.props.data.answer2 ? 
                                             <i className="material-icons">radio_button_checked</i> :
@@ -64,16 +73,19 @@ export default class Quiz extends Component {
                                         <p>{this.props.data.answer2}</p>
                                     </div>
                                 </label>
+
                             </form>
                         </div>
-
+                        {/*{Hide the submit button when they submit an answer. Disable it until they have selected something.}*/}
                         <div className="quiz-content__submit" style={{visibility: this.state.submitted ? "hidden" : "visible"}}>
                             <input type="submit" disabled={this.state.selected === ""} onClick={this.handleClick} />
                         </div>
 
+                {/*{Only show this div when the submit button is clicked.}*/}
                 { this.state.submitted &&
                     <div className="quiz-content__bottom">
                         <div className="quiz-content__results">
+                            {/*{If it's correct, show one div. If not, show the other.}*/}
                             {
                                 this.state.correct ?
                                     <div className="quiz-content__results--correct">
@@ -89,6 +101,7 @@ export default class Quiz extends Component {
                                     </div>
                             }
                         </div>
+                        {/*{"Try again"}*/}
                         <div className="quiz-content__results--again">
                             <button onClick={this.handleReset}><span><i className="material-icons">replay</i><p>take again</p></span>
                             </button>
